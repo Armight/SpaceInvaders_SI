5cc9cbe3d201af3afe63c26517dc3096fa68fef0
elaia <elaia@LAPTOP-GUE1O99A>
2026-03-04 20:55:42 +0100

    JokalariTxikia 4 pixelak jarri

--- SpaceInvaders/src/modelPackage/JokalariTxikia.java
+++ SpaceInvaders/src/modelPackage/JokalariTxikia.java
@@ -10,30 +10,30 @@ public class JokalariTxikia extends Jokalari {//4 pixeleko ontzia
 		
 	}
 
-	public void sortuJokalari4pixel(int pX, int pY) {
+	public void sortuJokalari4pixel(int pX, int pY) {//ontzia sortu koord hoietan
 		
 		GelaxkaMatrizea espazioa=GelaxkaMatrizea.getGelaxkaMatrizea();
-		if(pX-1>0) {
-			espazioa.getGelaxka(pX, pY).setEgoera("Jokalaria");
+		
+			espazioa.getGelaxka(pX, pY).setEgoera("Jokalaria"); //ontziaren erdiko pixela
 			
 		
-			espazioa.getGelaxka(pX-1, pY).setEgoera("Jokalaria");
+			espazioa.getGelaxka(pX-1, pY).setEgoera("Jokalaria");//ontziaren ezkerreko pixela
 			
 		
-			espazioa.getGelaxka(pX+1, pY).setEgoera("Jokalaria");
+			espazioa.getGelaxka(pX+1, pY).setEgoera("Jokalaria");//ontziaren eskumako pixela
 			
 		
-			espazioa.getGelaxka(pX, pY+1).setEgoera("Jokalaria");
+			espazioa.getGelaxka(pX, pY+1).setEgoera("Jokalaria");//ontziaren goiko pixela
 			
 		}
 		
 		
-	}
+	
 	
 	
 	protected void mugituEzkerrera4Pixel() {
 		GelaxkaMatrizea espazioa=GelaxkaMatrizea.getGelaxkaMatrizea();
-		if(pX-1>0) {
+		if(this.getX()-1>0) {
 			espazioa.getGelaxka(pX, pY).setEgoera("Jokalaria");
 			
 		
