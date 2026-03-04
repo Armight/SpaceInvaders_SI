944771147be560aac73a0334f2fcff6d8006db45
ahern <ahern@192.168.1.137>
2026-03-04 22:59:49 +0100

    tiro klasean eta azpiklasean aldaketak

--- SpaceInvaders/src/modelPackage/Tiro.java
+++ SpaceInvaders/src/modelPackage/Tiro.java
@@ -1,10 +1,19 @@
 package modelPackage;
 
 public abstract class Tiro extends Mugimendu {
-
+    
 	protected Tiro(int pX, int pY, boolean pErakutsi) {
-		super(pX, pY, pErakutsi);
-	
+		super(pX, pY, pErakutsi); //poner true porque siempre se crea visible no?
 	}
+	    
+    public void mugitu() {
+        int newY = getY() - 1;
+        setPosizio(getX(), newY);
+        
+        if (newY < 0) {
+            setIkusmena(false);
+        }
+    }
+
 
 }
--- SpaceInvaders/src/modelPackage/TiroTxikia.java
+++ SpaceInvaders/src/modelPackage/TiroTxikia.java
@@ -1,10 +1,8 @@
 package modelPackage;
 
 public class TiroTxikia extends Tiro {
-	private static int pixelKop=1;
 
-	public TiroTxikia() {
-		// TODO Auto-generated constructor stub
-	}
-
+    public TiroTxikia(int pX, int pY) {
+        super(pX, pY, true);
+    }
 }
