fe64ace787e7d9662e530db9168ed705858640dc
elaia <elaia@LAPTOP-GUE1O99A>
2026-03-04 14:45:17 +0100

    4 pixeleko ontzia

--- SpaceInvaders/src/modelPackage/JokalariTxikia.java
+++ SpaceInvaders/src/modelPackage/JokalariTxikia.java
@@ -1,18 +1,36 @@
 package modelPackage;
 
 public class JokalariTxikia extends Jokalari {//4 pixeleko ontzia
+	
+	
+	
 
 	public JokalariTxikia(int pX, int pY, boolean pErakutsi, int pixelKop) {
 		super(pX, pY, pErakutsi, pixelKop);
 		
 	}
 
-	public void sortuJokalari4pixel() {
-		int i=0;
+	public void sortuJokalari4pixel(int pX, int pY) {
+		int i=1;
 		GelaxkaMatrizea espazioa=GelaxkaMatrizea.getGelaxkaMatrizea();
-		while (i<4) {
-			
+		if(i==1) {//ontziaren erdiko pixel
+			espazioa.getGelaxka(pX, pY).setEgoera("Jokalaria");
+			i++;
 		}
+		else if (i==2) {//ontziaren ezkerreko pixel
+			espazioa.getGelaxka(pX-1, pY).setEgoera("Jokalaria");
+			i++;
+		}
+		else if (i==3) {//ontziaren eskumako pixel
+			espazioa.getGelaxka(pX+1, pY).setEgoera("Jokalaria");
+			i++;
+		}
+		else if (i==4) {//ontziaren goiko pixel
+			espazioa.getGelaxka(pX, pY+1).setEgoera("Jokalaria");
+			i++;
+		}
+		
+		
 	}
 	
 	protected void mugituEzkerrera4Pixel(GelaxkaMatrizea espazio) {
