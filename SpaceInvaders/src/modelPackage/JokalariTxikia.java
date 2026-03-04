4bd9c13cfa450f55ac9f694b0c945d3498be4dd9
elaia <elaia@LAPTOP-GUE1O99A>
2026-03-04 14:57:28 +0100

    4 pixeleko ontzia ONDO

--- SpaceInvaders/src/modelPackage/JokalariTxikia.java
+++ SpaceInvaders/src/modelPackage/JokalariTxikia.java
@@ -11,39 +11,48 @@ public class JokalariTxikia extends Jokalari {//4 pixeleko ontzia
 	}
 
 	public void sortuJokalari4pixel(int pX, int pY) {
-		int i=1;
+		
 		GelaxkaMatrizea espazioa=GelaxkaMatrizea.getGelaxkaMatrizea();
-		if(i==1) {//ontziaren erdiko pixel
+		if(pX-1>0) {
 			espazioa.getGelaxka(pX, pY).setEgoera("Jokalaria");
-			i++;
-		}
-		else if (i==2) {//ontziaren ezkerreko pixel
+			
+		
 			espazioa.getGelaxka(pX-1, pY).setEgoera("Jokalaria");
-			i++;
-		}
-		else if (i==3) {//ontziaren eskumako pixel
+			
+		
 			espazioa.getGelaxka(pX+1, pY).setEgoera("Jokalaria");
-			i++;
-		}
-		else if (i==4) {//ontziaren goiko pixel
+			
+		
 			espazioa.getGelaxka(pX, pY+1).setEgoera("Jokalaria");
-			i++;
+			
 		}
 		
 		
 	}
 	
-	protected void mugituEzkerrera4Pixel(GelaxkaMatrizea espazio) {
-		if (super.getX()>0) {
-			int i=0;
-			while (i<4) {
+	
+	protected void mugituEzkerrera4Pixel() {
+		if (super.getX()-1>0) {
+			int i=1;
+			GelaxkaMatrizea espazioa=GelaxkaMatrizea.getGelaxkaMatrizea();
+			if(i==1) {//ontziaren eskumako pixel kendu
+				espazioa.getGelaxka(this.getX()+1, this.getY()).setEgoera("Hutsik");
 				
+				i++;
 			}
-			Gelaxka g=GelaxkaMatrizea.getGelaxkaMatrizea().getGelaxka(this.getX(), this.getY());
-			g.setEgoera("Hutsik");
+			else if (i==2) {//ontziaren ezkerreko pixel
+				espazioa.getGelaxka(pX-1, pY).setEgoera("Jokalaria");
+				i++;
+			}
+			else if (i==3) {//ontziaren eskumako pixel
+				espazioa.getGelaxka(pX+1, pY).setEgoera("Jokalaria");
+				i++;
+			}
+			else if (i==4) {//ontziaren goiko pixel
+				espazioa.getGelaxka(pX, pY+1).setEgoera("Jokalaria");
+				i++;
+			}
 			
-			this.setPosizio(getX()-1, getY());
-			GelaxkaMatrizea.getGelaxkaMatrizea().getGelaxka(getX(), getY()).setEgoera("Jokalaria");}
 		}
-
+	}
 }
