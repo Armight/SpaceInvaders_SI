76ee9937338cf179d2d79e13e6ca83b688adfe1c
Armight <122396609+Armight@users.noreply.github.com>
2026-03-05 15:48:50 +0100

    tiro klasean aldaketak

--- SpaceInvaders/src/modelPackage/Tiro.java
+++ SpaceInvaders/src/modelPackage/Tiro.java
@@ -1,19 +1,17 @@
 package modelPackage;
 
 public abstract class Tiro extends Mugimendu {
-    
-	protected Tiro(int pX, int pY) {
-		super(pX, pY, true); //poner true porque siempre se crea visible
-	}
-	    
+
+    protected Tiro(int pX, int pY,int pPixelKop) {
+        super(pX, pY, true, pPixelKop);  // true=visible, 1=pixel bakarra Sprint 1ean
+    }
+
     public void mugitu() {
         int newY = getY() - 1;
         setPosizio(getX(), newY);
-        
+
         if (newY < 0) {
             setIkusmena(false);
         }
     }
-
-
 }
