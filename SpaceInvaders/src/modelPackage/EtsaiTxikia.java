a80887e54d71fc1bc2b475036dadf6301f9b6ca5
elaia <elaia@LAPTOP-GUE1O99A>
2026-03-06 13:12:26 +0100

    deskribapenak

--- SpaceInvaders/src/modelPackage/EtsaiTxikia.java
+++ SpaceInvaders/src/modelPackage/EtsaiTxikia.java
@@ -2,10 +2,8 @@ package modelPackage;
 
 public class EtsaiTxikia extends Etsai {
 	
-	
-
-
-    private static int pixelKop = 4;
+    private static int pixelKop = 4;//etsai txikia 4 pixeleko etsaia da
+    
     //eraikitzailea ondo ipini
     public EtsaiTxikia(int pX, int pY) {
         super(pX, pY, true, pixelKop,1);//etsai txikiaren bizitza=1
