ef24ec144fc7e1dd9442bb5a38d0ee233a7eb862
Armight <detalydecual@gmail.com>
2026-03-14 21:35:44 +0100

    Hau izan da gauza txiki batzuk txukun ipintzeko mainean

--- SpaceInvaders/src/kudeaketaPackage/main.java
+++ SpaceInvaders/src/kudeaketaPackage/main.java
@@ -2,8 +2,6 @@ package kudeaketaPackage;
 
 import java.awt.EventQueue;
 
-import java.util.ArrayList;
-
 import modelPackage.PartidaKudeatzailea;
 import viewPackage.AmaieraPantaila;
 import viewPackage.EspazioaView;
@@ -11,7 +9,7 @@ import viewPackage.HasieraPantaila;
 
 public class main {
     public static void main(String[] args) {
-    	HasieraPantaila hasierapnataila = new HasieraPantaila();
+    	HasieraPantaila hasieraPantaila = new HasieraPantaila();
     	EspazioaView espazioView = new EspazioaView();
     	AmaieraPantaila amaieraPantaila = new AmaieraPantaila();
     	EventQueue.invokeLater(() -> PartidaKudeatzailea.getPartidaKudeatzailea());
