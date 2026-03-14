2fc563e5033f1f473724fbbd533d326ede7bef13
IoritzLopez <lopezioritz13@gmail.com>
2026-03-14 19:05:40 +0100

    Multiteklak implementatuta

--- SpaceInvaders/src/viewPackage/Controller.java
+++ SpaceInvaders/src/viewPackage/Controller.java
@@ -1,21 +1,28 @@
 package viewPackage;
 
 import java.awt.Color;
-
 import java.awt.event.*;
+import java.util.HashSet;
+import java.util.Set;
+import javax.swing.Timer;
 
 import modelPackage.EspazioModel;
-import modelPackage.Jokalari;
 import modelPackage.PartidaKudeatzailea;
 
 public class Controller implements KeyListener {
     private static Controller nC;
-    //private Timer timer;
+    private Set<Integer> teklakSakatuta = new HashSet<>();
+    private Timer inputTimer;
+    
 
     private Controller() {
-    	//Timer-a espazioModel-ean sartuko dugu ^-^
-        //timer = new Timer(200, this);
-        //timer.start();
+    	inputTimer = new Timer(50, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                prozesatuInputak();
+            }
+        });
+        inputTimer.start();
     }
 
     public static Controller getController() {
@@ -48,34 +55,38 @@ public class Controller implements KeyListener {
             //Hau da ESC teklarako (AmaieraPantaila klasean hobeto azalduta T-T)
             case KeyEvent.VK_ESCAPE:
             	PartidaKudeatzailea.getPartidaKudeatzailea().irteeraEgin();
-                break;
+            default:
+            	teklakSakatuta.add(e.getKeyCode());
         }
         
         //ALDAKETAK: hemen ezin da egon modeloko objekturik
 
         
         
-            if (e.getKeyCode() == KeyEvent.VK_UP || e.getKeyCode() == KeyEvent.VK_W) {
-                EspazioModel.getGelaxkaMatrizea().mugituJokalariaY(1); 
-            }
-            if (e.getKeyCode() == KeyEvent.VK_DOWN || e.getKeyCode() == KeyEvent.VK_S) {
-            	EspazioModel.getGelaxkaMatrizea().mugituJokalariaY(-1); 
-            }
-            if (e.getKeyCode() == KeyEvent.VK_LEFT || e.getKeyCode() == KeyEvent.VK_A) {
-            	EspazioModel.getGelaxkaMatrizea().mugituJokalariaX(-1);
-            }
-            if (e.getKeyCode() == KeyEvent.VK_RIGHT || e.getKeyCode() == KeyEvent.VK_D) {
-            	EspazioModel.getGelaxkaMatrizea().mugituJokalariaX(1); 
-            }
-            if (e.getKeyCode() == KeyEvent.VK_SPACE) {
-            	EspazioModel.getGelaxkaMatrizea().shoot(); 
-            }
+}
         
+    
+    private void prozesatuInputak() {
+    	if (teklakSakatuta.contains(KeyEvent.VK_UP) || teklakSakatuta.contains(KeyEvent.VK_W)) {
+            EspazioModel.getGelaxkaMatrizea().mugituJokalariaY(1); 
+        }
+        if (teklakSakatuta.contains(KeyEvent.VK_DOWN) || teklakSakatuta.contains(KeyEvent.VK_S)) {
+        	EspazioModel.getGelaxkaMatrizea().mugituJokalariaY(-1); 
+        }
+        if (teklakSakatuta.contains(KeyEvent.VK_LEFT)  || teklakSakatuta.contains(KeyEvent.VK_A)) {
+        	EspazioModel.getGelaxkaMatrizea().mugituJokalariaX(-1);
+        }
+        if (teklakSakatuta.contains(KeyEvent.VK_RIGHT)  || teklakSakatuta.contains(KeyEvent.VK_D) ) {
+        	EspazioModel.getGelaxkaMatrizea().mugituJokalariaX(1); 
+        }
+        if (teklakSakatuta.contains(KeyEvent.VK_SPACE)  ) {
+        	EspazioModel.getGelaxkaMatrizea().shoot(); 
+        }
     }
 
     @Override
-    public void keyReleased(KeyEvent e) {}
-
+    public void keyReleased(KeyEvent e) {teklakSakatuta.remove(e.getKeyCode());} //pultsatutako teklak ezabatzeko
+    	
     /*@Override
     public void actionPerformed(ActionEvent e) {
         EspazioModel.getGelaxkaMatrizea().jokoaEguneratu();
