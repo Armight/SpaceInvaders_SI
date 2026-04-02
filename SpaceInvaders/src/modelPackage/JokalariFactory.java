f2e7f7a9572f283b7f8baab111dc49da64ce28d8
Armight <detalydecual@gmail.com>
2026-04-02 20:51:20 +0200

    FACTORY PATROIAREN IMPLEMENTAZIOAREN HASIERA (EZ DAGO AMAITUTA) factory klasea eginda jokalariarentzako

---
+++ SpaceInvaders/src/modelPackage/JokalariFactory.java
@@ -0,0 +1,29 @@
+package modelPackage;
+
+public class JokalariFactory {
+	private static JokalariFactory nJokalariFactory;
+	
+	private JokalariFactory() {}
+	
+	public static JokalariFactory getJK() {
+		if (nJokalariFactory==null) {
+			nJokalariFactory = new JokalariFactory();
+		}
+		return nJokalariFactory;
+	}
+	
+	public Jokalari createJokalaria(String pKolorea, int pX, int pY) {
+		Jokalari jokalaria = null;
+		if (pKolorea.equals("GREEN")) {
+			jokalaria = new Jokalari(pX,pY);
+		}
+		if (pKolorea.equals("RED")) {
+			jokalaria = new Jokalari(pX,pY);
+		}
+		if (pKolorea.equals("BLUE")) {
+			jokalaria = new Jokalari(pX,pY);
+		}
+		return jokalaria;
+	}
+	
+}
