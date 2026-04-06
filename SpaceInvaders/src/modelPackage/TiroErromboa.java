4dca1447cb8913d3965f9ae4020efe3309c386bb
Armight <detalydecual@gmail.com>
2026-04-06 16:33:11 +0200

    COMPOSITEN ALDAKETAK
    Hosto klaseen sorrera eta egituraketa

---
+++ SpaceInvaders/src/modelPackage/TiroErromboa.java
@@ -0,0 +1,24 @@
+package modelPackage;
+
+public class TiroErromboa extends TiroTaldea {
+    public TiroErromboa(int pX, int pY) {
+        
+    	this.addTiro(new Tiro(pX, pY));
+
+    	this.addTiro(new Tiro(pX-1, pY-1));
+    	this.addTiro(new Tiro(pX, pY-1));
+    	this.addTiro(new Tiro(pX+1, pY-1));
+
+    	this.addTiro(new Tiro(pX-2, pY-2));
+    	this.addTiro(new Tiro(pX-1, pY-2));
+    	this.addTiro(new Tiro(pX, pY-2));
+    	this.addTiro(new Tiro(pX+1, pY-2));
+    	this.addTiro(new Tiro(pX+2, pY-2));
+
+    	this.addTiro(new Tiro(pX-1, pY-3));
+    	this.addTiro(new Tiro(pX, pY-3));
+    	this.addTiro(new Tiro(pX+1, pY-3));
+
+    	this.addTiro(new Tiro(pX, pY-4));
+    }
+}
---
+++ SpaceInvaders/src/modelPackage/TiroGezia.java
@@ -0,0 +1,11 @@
+package modelPackage;
+
+public class TiroGezia extends TiroTaldea {
+    
+	public TiroGezia(int pX, int pY) {
+		
+        this.addTiro(new Tiro(pX, pY)); 
+        this.addTiro(new Tiro(pX - 1, pY - 1));
+        this.addTiro(new Tiro(pX + 1, pY - 1));
+    }
+}
