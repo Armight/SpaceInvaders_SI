0dc634f9398bda68813538a19a07a218dac8e992
Armight <detalydecual@gmail.com>
2026-04-08 16:14:31 +0200

    ALDAKETA TXIKIAK STRATEGY-n
    Gauza txiki batzuk ahaztu zaizkit

--- SpaceInvaders/src/modelPackage/TiroBakarraPortaera.java
+++ SpaceInvaders/src/modelPackage/TiroBakarraPortaera.java
@@ -2,7 +2,7 @@ package modelPackage;
 
 public class TiroBakarraPortaera implements TiroPortaera{
 	public void shoot(int pX, int pY) {
-		Tiro tiro = new Tiro(pX, pY);
+		Tiro tiro = new Tiro(pX, pY-2);
 		EspazioModel.getGelaxkaMatrizea().addTiro(tiro);
 	}
 }
--- SpaceInvaders/src/modelPackage/TiroErromboaPortaera.java
+++ SpaceInvaders/src/modelPackage/TiroErromboaPortaera.java
@@ -2,7 +2,7 @@ package modelPackage;
 
 public class TiroErromboaPortaera implements TiroPortaera{
 	public void shoot(int pX, int pY) {
-		TiroErromboa tiro = new TiroErromboa(pX, pY);
+		TiroErromboa tiro = new TiroErromboa(pX, pY-2);
 		EspazioModel.getGelaxkaMatrizea().addTiro(tiro);
 	}
 
