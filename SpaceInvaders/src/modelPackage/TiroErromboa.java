06bad1f79fcd40d0101e8b8402f2283e5eadf4f5
IoritzLopez <lopezioritz13@gmail.com>
2026-04-10 19:50:35 +0200

    TiroErromboa OutOfBounds ez emateko sortzen den lekua matrizetik kanpo badago.

--- SpaceInvaders/src/modelPackage/TiroErromboa.java
+++ SpaceInvaders/src/modelPackage/TiroErromboa.java
@@ -5,16 +5,20 @@ public class TiroErromboa extends TiroMultipixel {
     public TiroErromboa(int pX, int pY) {
         
     	super(pX, pY);
+    	if(pY >= 6) { //OutOfBounds ez emateko goi goian sortzen dugunean tiroa.
+    		int[][] posizioak = new int[][] {
+        		{0, 0}, {0, -1}, {1, -1}, {-1, -1},
+        	    {0, -2}, {1, -2}, {-1, -2}, {-2, -2}, {2, -2},
+        	    {0, -3}, {1, -3}, {-1, -3}, {0, -4}};
+        	
+        	
+        	for (int[] pos : posizioak) {
+        		Tiro t = new Tiro(pX + pos[0], pY + pos[1]);
+        		this.addTiro(t);
     	
-    	int[][] posizioak = new int[][] {
-    		{0, 0}, {0, -1}, {1, -1}, {-1, -1},
-    	    {0, -2}, {1, -2}, {-1, -2}, {-2, -2}, {2, -2},
-    	    {0, -3}, {1, -3}, {-1, -3}, {0, -4}};
+    		
+    	}
     	
-    	
-    	for (int[] pos : posizioak) {
-    		Tiro t = new Tiro(pX + pos[0], pY + pos[1]);
-    		this.addTiro(t);
     	}
     }
 }
