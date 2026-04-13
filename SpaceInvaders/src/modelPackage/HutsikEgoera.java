d72b3b396430f5c33957463df47471a8f30fa62b
elaia <elaia@LAPTOP-GUE1O99A>
2026-04-13 22:58:49 +0200

    Hutsik

--- SpaceInvaders/src/modelPackage/HutsikEgoera.java
+++ SpaceInvaders/src/modelPackage/HutsikEgoera.java
@@ -1,6 +1,8 @@
 package modelPackage;
 
 public class HutsikEgoera implements Egoera {
+	
+	public HutsikEgoera() {};
     @Override
     public void setEgoera(Gelaxka g) {
         g.setEgoera(this);
